701c91f1f924284867e5f6ce7a256642007ba1aa
Guru <gurumadhav08@gmail.com>
2019-11-20 23:11:49 +0000

    added some more text in func2()

--- Example.py
+++ Example.py
@@ -2,7 +2,7 @@ def func():
 	print("this is a example file in proj directory")
 
 def func2():
-	print("this is second function in example file")
+	print("this is second function in example file and now added some text to see the difference")
 
 
 func()
