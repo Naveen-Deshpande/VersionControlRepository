d4b55e9438f49bfd6263d94e96aac66acfdfbe72
Guru <gurumadhav08@gmail.com>
2019-11-21 12:48:03 +0000

    added sqroot function

--- Example2.py
+++ Example2.py
@@ -10,7 +10,11 @@ def num_sq(n):
 def num_cube(n):
 	return n**3
 
+def num_sqroot(n):
+	return n**0.5
+
 add_res = add(10,20)
 mul_res = mul(5,10)
 sq_res = num_sq(5)
 cube_res = num_cube(3)
+sqroot_res = num_sqroot(4)
